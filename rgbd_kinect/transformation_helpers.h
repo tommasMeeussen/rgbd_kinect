02602bdb14ed516e0c58d6bacc59537e1778523a
Tommas Meeussen <meeusset@sheridancollege.ca>
2021-10-07 14:49:38 -0400

    Add project files.

---
+++ rgbd_kinect/transformation_helpers.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <k4a/k4a.h>
+
+void tranformation_helpers_write_point_cloud(const k4a_image_t point_cloud_image,
+    const k4a_image_t color_image,
+    const char* file_name);
+
+k4a_image_t downscale_image_2x2_binning(const k4a_image_t color_image);
