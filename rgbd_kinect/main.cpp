efe3adfb2a01f5f775522fa674ef53f7c4983f57
Tommas Meeussen <meeusset@sheridancollege.ca>
2022-01-10 13:00:04 -0500

    Removed Capture function to improve readability

--- rgbd_kinect/main.cpp
+++ rgbd_kinect/main.cpp
@@ -6,6 +6,11 @@
 #include <string>
 #include "transformation_helpers.h"
 #include <turbojpeg.h>
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "iostream"
+#include "fstream"
+using namespace cv;
 
 static bool point_cloud_color_to_depth(k4a_transformation_t transformation_handle,
     const k4a_image_t depth_image,
@@ -116,173 +121,6 @@ static bool point_cloud_depth_to_color(k4a_transformation_t transformation_handl
     return true;
 }
 
-static int capture(std::string output_dir, uint8_t deviceId = K4A_DEVICE_DEFAULT)
-{
-    int returnCode = 1;
-    k4a_device_t device = NULL;
-    const int32_t TIMEOUT_IN_MS = 10000;
-    k4a_transformation_t transformation = NULL;
-    k4a_transformation_t transformation_color_downscaled = NULL;
-    k4a_capture_t capture = NULL;
-    std::string file_name = "";
-    uint32_t device_count = 0;
-    k4a_device_configuration_t config = K4A_DEVICE_CONFIG_INIT_DISABLE_ALL;
-    k4a_image_t depth_image = NULL;
-    k4a_image_t color_image = NULL;
-    k4a_image_t color_image_downscaled = NULL;
-
-    device_count = k4a_device_get_installed_count();
-
-    if (device_count == 0)
-    {
-        printf("No K4A devices found\n");
-        return 0;
-    }
-
-    if (K4A_RESULT_SUCCEEDED != k4a_device_open(deviceId, &device))
-    {
-        printf("Failed to open device\n");
-        goto Exit;
-    }
-
-    config.color_format = K4A_IMAGE_FORMAT_COLOR_BGRA32;
-    config.color_resolution = K4A_COLOR_RESOLUTION_720P;
-    config.depth_mode = K4A_DEPTH_MODE_NFOV_UNBINNED;
-    config.camera_fps = K4A_FRAMES_PER_SECOND_30;
-    config.synchronized_images_only = true; // ensures that depth and color images are both available in the capture
-
-    k4a_calibration_t calibration;
-    if (K4A_RESULT_SUCCEEDED !=
-        k4a_device_get_calibration(device, config.depth_mode, config.color_resolution, &calibration))
-    {
-        printf("Failed to get calibration\n");
-        goto Exit;
-    }
-
-    transformation = k4a_transformation_create(&calibration);
-
-    if (K4A_RESULT_SUCCEEDED != k4a_device_start_cameras(device, &config))
-    {
-        printf("Failed to start cameras\n");
-        goto Exit;
-    }
-
-    // Get a capture
-    switch (k4a_device_get_capture(device, &capture, TIMEOUT_IN_MS))
-    {
-    case K4A_WAIT_RESULT_SUCCEEDED:
-        break;
-    case K4A_WAIT_RESULT_TIMEOUT:
-        printf("Timed out waiting for a capture\n");
-        goto Exit;
-    case K4A_WAIT_RESULT_FAILED:
-        printf("Failed to read a capture\n");
-        goto Exit;
-    }
-
-    // Get a depth image
-    depth_image = k4a_capture_get_depth_image(capture);
-    if (depth_image == 0)
-    {
-        printf("Failed to get depth image from capture\n");
-        goto Exit;
-    }
-
-    // Get a color image
-    color_image = k4a_capture_get_color_image(capture);
-    if (color_image == 0)
-    {
-        printf("Failed to get color image from capture\n");
-        goto Exit;
-    }
-
-    // Compute color point cloud by warping color image into depth camera geometry
-#ifdef _WIN32
-    file_name = output_dir + "\\color_to_depth.ply";
-#else
-    file_name = output_dir + "/color_to_depth.ply";
-#endif
-    if (point_cloud_color_to_depth(transformation, depth_image, color_image, file_name.c_str()) == false)
-    {
-        goto Exit;
-    }
-
-    // Compute color point cloud by warping depth image into color camera geometry
-#ifdef _WIN32
-    file_name = output_dir + "\\depth_to_color.ply";
-#else
-    file_name = output_dir + "/depth_to_color.ply";
-#endif
-    if (point_cloud_depth_to_color(transformation, depth_image, color_image, file_name.c_str()) == false)
-    {
-        goto Exit;
-    }
-
-    // Compute color point cloud by warping depth image into color camera geometry with downscaled color image and
-    // downscaled calibration. This example's goal is to show how to configure the calibration and use the
-    // transformation API as it is when the user does not need a point cloud from high resolution transformed depth
-    // image. The downscaling method here is naively to average binning 2x2 pixels, user should choose their own
-    // appropriate downscale method on the color image, this example is only demonstrating the idea. However, no matter
-    // what scale you choose to downscale the color image, please keep the aspect ratio unchanged (to ensure the
-    // distortion parameters from original calibration can still be used for the downscaled image).
-    k4a_calibration_t calibration_color_downscaled;
-    memcpy(&calibration_color_downscaled, &calibration, sizeof(k4a_calibration_t));
-    calibration_color_downscaled.color_camera_calibration.resolution_width /= 2;
-    calibration_color_downscaled.color_camera_calibration.resolution_height /= 2;
-    calibration_color_downscaled.color_camera_calibration.intrinsics.parameters.param.cx /= 2;
-    calibration_color_downscaled.color_camera_calibration.intrinsics.parameters.param.cy /= 2;
-    calibration_color_downscaled.color_camera_calibration.intrinsics.parameters.param.fx /= 2;
-    calibration_color_downscaled.color_camera_calibration.intrinsics.parameters.param.fy /= 2;
-    transformation_color_downscaled = k4a_transformation_create(&calibration_color_downscaled);
-    color_image_downscaled = downscale_image_2x2_binning(color_image);
-    if (color_image_downscaled == 0)
-    {
-        printf("Failed to downscaled color image\n");
-        goto Exit;
-    }
-
-#ifdef _WIN32
-    file_name = output_dir + "\\depth_to_color_downscaled.ply";
-#else
-    file_name = output_dir + "/depth_to_color_downscaled.ply";
-#endif
-    if (point_cloud_depth_to_color(transformation_color_downscaled,
-        depth_image,
-        color_image_downscaled,
-        file_name.c_str()) == false)
-    {
-        goto Exit;
-    }
-
-    returnCode = 0;
-
-Exit:
-    if (depth_image != NULL)
-    {
-        k4a_image_release(depth_image);
-    }
-    if (color_image != NULL)
-    {
-        k4a_image_release(color_image);
-    }
-    if (capture != NULL)
-    {
-        k4a_capture_release(capture);
-    }
-    if (transformation != NULL)
-    {
-        k4a_transformation_destroy(transformation);
-    }
-    if (transformation_color_downscaled != NULL)
-    {
-        k4a_transformation_destroy(transformation_color_downscaled);
-    }
-    if (device != NULL)
-    {
-        k4a_device_close(device);
-    }
-    return returnCode;
-}
 
 // Timestamp in milliseconds. Defaults to 1 sec as the first couple frames don't contain color
 static int playback(char* input_path, int timestamp = 20000, std::string output_filename = "output.ply")
@@ -295,26 +133,33 @@ static int playback(char* input_path, int timestamp = 20000, std::string output_
     k4a_image_t depth_image = NULL;
     k4a_image_t color_image = NULL;
     k4a_image_t uncompressed_color_image = NULL;
+    k4a_image_t transformed_depth_image = NULL;
+    int color_width_pixels = 0;
+    int color_height_pixels = 0;
+    uint8_t* buffer = NULL;
+    int rows = NULL;
+    int cols = NULL;
+
 
     k4a_result_t result;
     k4a_stream_result_t stream_result;
 
     std::string dir = "c:\\users\\tommas\\kinect_transformations\\\\";
-    std::string filename = "output.ply";
+    std::string filename = "output2.png";
     std::string out_file = dir + filename;
     // open recording
     result = k4a_playback_open(input_path, &playback);
     if (result != K4A_RESULT_SUCCEEDED || playback == NULL)
     {
         printf("failed to open recording %s\n", input_path);
-        goto exit;
+        //goto exit;
     }
 
-    result = k4a_playback_seek_timestamp(playback, 20000 * 1000, K4A_PLAYBACK_SEEK_BEGIN);
+    result = k4a_playback_seek_timestamp(playback, 15000 * 1000, K4A_PLAYBACK_SEEK_BEGIN);
     if (result != K4A_RESULT_SUCCEEDED)
     {
         printf("failed to seek timestamp %d\n", 20000);
-        goto exit;
+        //goto exit;
     }
     printf("seeking to timestamp: %d/%d (ms)\n",
         20000,
@@ -324,13 +169,13 @@ static int playback(char* input_path, int timestamp = 20000, std::string output_
     if (stream_result != K4A_STREAM_RESULT_SUCCEEDED || capture == NULL)
     {
         printf("failed to fetch frame\n");
-        goto exit;
+        //goto exit;
     }
 
     if (K4A_RESULT_SUCCEEDED != k4a_playback_get_calibration(playback, &calibration))
     {
         printf("failed to get calibration\n");
-        goto exit;
+        //goto exit;
     }
 
     transformation = k4a_transformation_create(&calibration);
@@ -340,14 +185,14 @@ static int playback(char* input_path, int timestamp = 20000, std::string output_
     if (depth_image == 0)
     {
         printf("failed to get depth image from capture\n");
-        goto exit;
+        //goto exit;
     }
 
     color_image = k4a_capture_get_color_image(capture);
     if (color_image == 0)
     {
         printf("failed to get color image from capture\n");
-        goto exit;
+        //goto exit;
     }
 
     // convert color frame from mjpeg to bgra
@@ -356,7 +201,7 @@ static int playback(char* input_path, int timestamp = 20000, std::string output_
     if (format != K4A_IMAGE_FORMAT_COLOR_MJPG)
     {
         printf("color format not supported. please use mjpeg\n");
-        goto exit;
+        //goto exit;
     }
 
     int color_width, color_height;
@@ -370,7 +215,7 @@ static int playback(char* input_path, int timestamp = 20000, std::string output_
         &uncompressed_color_image))
     {
         printf("failed to create image buffer\n");
-        goto exit;
+        //goto exit;
     }
 
     tjhandle tjhandle;
@@ -390,7 +235,7 @@ static int playback(char* input_path, int timestamp = 20000, std::string output_
         {
             printf("failed to destroy turbojpeg handle\n");
         }
-        goto exit;
+        //goto exit;
     }
     if (tjDestroy(tjhandle))
     {
@@ -399,7 +244,7 @@ static int playback(char* input_path, int timestamp = 20000, std::string output_
 
     // compute color point cloud by warping depth image into color camera geometry
     //works but wrong file type
-    if (point_cloud_depth_to_color(transformation, depth_image, uncompressed_color_image, out_file) == false)
+   /* if (point_cloud_depth_to_color(transformation, depth_image, uncompressed_color_image, out_file) == false)
     {
         printf("failed to transform depth to color\n");
         goto exit;
@@ -408,24 +253,112 @@ static int playback(char* input_path, int timestamp = 20000, std::string output_
     {
         printf("it worked\n");
 
-    }
+    }*/
 
-   /* k4a_image_create(K4A_IMAGE_FORMAT_COLOR_BGRA32,
+   /*k4a_image_create(K4A_IMAGE_FORMAT_DEPTH16,
         color_width,
         color_height,
-        color_width * 4 * (int)sizeof(uint8_t),
-        &uncompressed_color_image);*/
+        color_width* (int)sizeof(uint16_t),
+        &transformed_depth_image);*/
+   printf("original image width: %d\n", k4a_image_get_width_pixels(depth_image));
+   printf("original image height: %d\n", k4a_image_get_width_pixels(depth_image));
 
-    //if (k4a_transformation_depth_image_to_color_camera(transformation, depth_image, uncompressed_color_image) == false)
-    //{
-    //    printf("failed to transform depth to color\n");
-    //    goto exit;
-    //}
-    //else
-    //{
-    //    printf("it worked\n");
 
-    //}
+   color_width_pixels = k4a_image_get_width_pixels(color_image);
+   color_height_pixels = k4a_image_get_height_pixels(color_image);
+   if (K4A_RESULT_SUCCEEDED != k4a_image_create(K4A_IMAGE_FORMAT_DEPTH16,
+       color_width_pixels,
+       color_height_pixels,
+       color_width_pixels * (int)sizeof(uint16_t),
+       &transformed_depth_image))
+   {
+       printf("Failed to create transformed depth image\n");
+       return false;
+   }
+   else {
+       printf("conversion completed\n");
+   }
+
+
+
+   int depth_image_width_pixels = k4a_image_get_width_pixels(depth_image);
+   int depth_image_height_pixels = k4a_image_get_height_pixels(depth_image);
+   k4a_image_t transformed_color_image = NULL;
+   if (K4A_RESULT_SUCCEEDED != k4a_image_create(K4A_IMAGE_FORMAT_COLOR_BGRA32,
+       depth_image_width_pixels,
+       depth_image_height_pixels,
+       depth_image_width_pixels * 4 * (int)sizeof(uint8_t),
+       &transformed_color_image))
+   {
+       printf("Failed to create transformed color image\n");
+       return false;
+   }
+
+   std::string dir2 = "c:\\users\\tommas\\kinect_transformations\\\\";
+   std::string filename2 = "output_RGBD.png";
+   std::string out_fileRGBD = dir2 + filename2;
+
+   int rows2 = k4a_image_get_height_pixels(transformed_color_image);
+   int cols2 = k4a_image_get_width_pixels(transformed_color_image);
+
+
+   k4a_transformation_color_image_to_depth_camera(transformation, depth_image, uncompressed_color_image, transformed_color_image);
+
+   uint8_t* rgbd_buffer = k4a_image_get_buffer(transformed_color_image);
+   cv::Mat rgbdMat(512, 512, CV_8UC4, (void*)rgbd_buffer, cv::Mat::AUTO_STEP);
+   imwrite(out_fileRGBD, rgbdMat);
+
+
+
+   k4a_transformation_depth_image_to_color_camera(transformation, depth_image, transformed_depth_image);
+
+   //k4a_transformation_color_image_to_depth_camera(transformation, color_image, transformed_color_image);
+
+   buffer = k4a_image_get_buffer(transformed_depth_image);
+
+   
+
+   // convert the raw buffer to cv::Mat
+   rows = k4a_image_get_height_pixels(transformed_depth_image);
+   cols = k4a_image_get_width_pixels(transformed_depth_image);
+   //LinearStretching(rows, cols);
+   cv::Mat depthMat(rows, cols, CV_16UC1, (void*)buffer, cv::Mat::AUTO_STEP);
+   printf("transformed image width: %d\n", k4a_image_get_width_pixels(transformed_depth_image));
+   printf("transformed image height: %d\n", k4a_image_get_height_pixels(transformed_depth_image));
+
+   imwrite(out_file, depthMat);
+
+   
+  /* size_t image_buffer_size = k4a_image_get_size(transformed_depth_image);
+   std::ofstream file_object("hello_rgb.png", std::ios::out | std::ios::binary);
+   file_object.write(reinterpret_cast<char*>(buffer), image_buffer_size);
+   file_object.close();*/
+
+
+  
+
+   
+
+   
+
+   /*if (k4a_transformation_depth_image_to_color_camera(transformation, depth_image, transformed_depth_image))
+   {
+       printf("yay");
+   }
+   else {
+       printf(":(");
+   }*/
+
+   /* if (k4a_transformation_depth_image_to_color_camera(transformation, depth_image, depth_color_image) == false)
+    {
+        printf("failed to transform depth to color\n");
+        goto exit;
+    }
+    else
+    {
+        printf("it worked\n");
+
+    }*/
 
    // if (k4a_transformation_depth_image_to_color_camera(transformation, depth_image, uncompressed_color_image) == false)
    //{
@@ -486,22 +419,7 @@ int main(int argc, char** argv)
     else
     {
         std::string mode = std::string(argv[1]);
-        if (mode == "capture")
-        {
-            if (argc == 3)
-            {
-                returnCode = capture(argv[2]);
-            }
-            else if (argc == 4)
-            {
-                returnCode = capture(argv[2], (uint8_t)atoi(argv[3]));
-            }
-            else
-            {
-                print_usage();
-            }
-        }
-        else if (mode == "playback")
+      if (mode == "playback")
         {
             if (argc == 3)
             {
